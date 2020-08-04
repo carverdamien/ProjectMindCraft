0379ad1499f917c16682cc9bb7bad7c60639a56a
Damien Carver <carverdamien@gmail.com>
2020-08-04 08:58:26 +0200

    example.c

---
+++ example.c
@@ -0,0 +1,27 @@
+#include<mindcraft/game0/bot.h>
+#include<mindcraft/game0/rng.h>
+
+#define RRANGE 1000
+
+uint64_t random_bot(struct bot_t *bot) {
+	uint32_t r = ((uint32_t) rng(&bot->rw.random)) % (RRANGE*100);
+
+	if (r < (RRANGE*50) ) {
+		/* 50% */
+		bot->rw.action = BOT_WALK;
+		return 0;
+	}
+	if (r < (RRANGE*70)) {
+		/* 20% */
+		bot->rw.action = BOT_TURN_LEFT;
+		return 0;
+	}
+	if (r < (RRANGE*90)) {
+		/* 20% */
+		bot->rw.action = BOT_TURN_RIGHT;
+		return 0;
+	}
+	/* 10% */
+	bot->rw.action =  BOT_WAIT;
+	return 0;
+}
