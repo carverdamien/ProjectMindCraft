3d711bbfb9618cab249f0e3242f3e91a8828620e
Damien Carver <carverdamien@gmail.com>
2020-08-04 09:09:09 +0200

    rng.h

---
+++ rng.h
@@ -0,0 +1,10 @@
+#ifndef _RNG_H_
+#define _RNG_H_
+
+static __attribute__((always_inline)) int32_t rng(int32_t *random)
+{
+	*random = ((*random * 1103515245U) + 12345U) & 0x7fffffff;
+	return *random;
+}
+
+#endif
