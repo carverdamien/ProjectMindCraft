be1717d24cb3d48a08ffcd0e108173940c44b318
Damien Carver <carverdamien@gmail.com>
2020-08-06 12:08:14 +0200

    bots/spiral_bot.c

---
+++ bots/spiral_bot.c
@@ -0,0 +1,16 @@
+#include <mindcraft/game0/bot.h>
+
+#define SQRT_NR_BOT 10
+#define X0 (SQRT_NR_BOT/2)
+#define Z0 (SQRT_NR_BOT/2)
+
+uint64_t spiral_bot(struct bot_t *bot) {
+	/* 
+	 * Coding Challenge:
+	 *
+	 * Write a program that will make the bot spiral towards (X0,Z0).
+	 * Submit your solution with a pull request.
+	 *
+	 */
+	return 0;
+}
