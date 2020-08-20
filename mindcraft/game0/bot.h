8e0efc9ff5f615c9b50a09ee2b433651ec933ad9
Damien Carver <carverdamien@gmail.com>
2020-08-20 15:43:06 +0200

    update bot.h

--- mindcraft/game0/bot.h
+++ mindcraft/game0/bot.h
@@ -4,37 +4,107 @@
 #include <mindcraft/types.h>
 #include <mindcraft/game0/physics.h>
 
+/*
+enum bot_action_t:
+The value is written by the bot's program.
+The value is read by compute_interactions and compute_physics.
+
+Dev Notes:
+The value is also written by unittests.
+The value is also read by apply_reaction.
+*/
+
 enum bot_action_t {
-	BOT_WAIT,
-	BOT_WALK,
-	BOT_TURN_RIGHT,
-	BOT_TURN_LEFT,
-	BOT_NR_ACTION,
+
+	/* Interactions */
+	BOT_ACTION_HARVEST,
+	/* Future Interactions */
+	// BOT_ACTION_BUILD
+	// BOT_ACTION_REPAIR
+	// BOT_ACTION_ATTACK
+	// BOT_ACTION_DEFEND
+	// BOT_ACTION_DODGE
+	// BOT_ACTION_HELP
+
+	/* Physics */
+	BOT_ACTION_WAIT,
+	BOT_ACTION_WALK,
+	BOT_ACTION_TURN_RIGHT,
+	BOT_ACTION_TURN_LEFT,
+	/* Future Physics */
+	// BOT_ACTION_HOVER
+	// BOT_ACTION_LAND
+	// BOT_ACTION_SUBMERGE
+	// BOT_ACTION_SURFACE
+
+	/* Total */
+	NR_BOT_ACTION,
+
+	/* Deprecated Symbols */
+	BOT_WAIT = BOT_ACTION_WAIT,
+	BOT_WALK = BOT_ACTION_WALK,
+	BOT_TURN_RIGHT = BOT_ACTION_TURN_RIGHT,
+	BOT_TURN_LEFT = BOT_ACTION_TURN_LEFT,
+	BOT_NR_ACTION = NR_BOT_ACTION,
+
 } __attribute__ ((__packed__));
 ASSERT(sizeof(enum bot_action_t) == 1);
 
+/*
+enum bot_error_t:
+The value is reset to SUCESS by prepare_bots.
+The value is written by compute_interactions and compute_physics.
+The value can be read by the bot's program.
+*/
+
 enum bot_error_t {
 	BOT_SUCCESS,
+	BOT_INTERACTIONS_ERROR,
 	BOT_PHYSICS_ERROR,
 } __attribute__ ((__packed__));
 ASSERT(sizeof(enum bot_error_t) == 1);
 
+/*
+enum bot_reaction_t:
+These values correspond to the animations displayed in the unity client.
+*/
+
 enum bot_reaction_t {
-	BOT_IDLE,
-	BOT_MOVE_FORWARD_SUCCEEDED,
-	BOT_MOVE_FORWARD_FAILED_COLLISION_AT_EDGE,
-	BOT_MOVE_FORWARD_FAILED_COLLISION_AT_CENTER,
-	BOT_ROTATE_LEFT,
-	BOT_ROTATE_RIGHT,
-	BOT_NR_REACTION,
+
+	BOT_REACTION_IDLE,
+	BOT_REACTION_MOVE_FORWARD_SUCCEEDED,
+	BOT_REACTION_MOVE_FORWARD_FAILED_COLLISION_AT_EDGE,
+	BOT_REACTION_MOVE_FORWARD_FAILED_COLLISION_AT_CENTER,
+	BOT_REACTION_ROTATE_LEFT,
+	BOT_REACTION_ROTATE_RIGHT,
+	BOT_REACTION_HARVEST_SUCCEEDED,
+	BOT_REACTION_HARVEST_FAILED,
+	NR_BOT_REACTION,
+
+	/* Deprecated Symbols */
+	BOT_IDLE = BOT_REACTION_IDLE,
+	BOT_MOVE_FORWARD_SUCCEEDED = BOT_REACTION_MOVE_FORWARD_SUCCEEDED,
+	BOT_MOVE_FORWARD_FAILED_COLLISION_AT_EDGE = BOT_REACTION_MOVE_FORWARD_FAILED_COLLISION_AT_EDGE,
+	BOT_MOVE_FORWARD_FAILED_COLLISION_AT_CENTER = BOT_REACTION_MOVE_FORWARD_FAILED_COLLISION_AT_CENTER,
+	BOT_ROTATE_LEFT = BOT_REACTION_ROTATE_LEFT,
+	BOT_ROTATE_RIGHT = BOT_REACTION_ROTATE_RIGHT,
+	BOT_NR_REACTION = NR_BOT_REACTION,
+
 } __attribute__ ((__packed__));
 ASSERT(sizeof(enum bot_reaction_t) == 1);
 
 enum bot_orientation_t {
-	EAST,
-	NORTH,
-	WEST,
-	SOUTH,
+	BOT_ORIENTATION_EAST,
+	BOT_ORIENTATION_NORTH,
+	BOT_ORIENTATION_WEST,
+	BOT_ORIENTATION_SOUTH,
+
+	/* Deprecated Symbols */
+	EAST = BOT_ORIENTATION_EAST,
+	NORTH = BOT_ORIENTATION_NORTH,
+	WEST = BOT_ORIENTATION_WEST,
+	SOUTH = BOT_ORIENTATION_SOUTH,
+
 } __attribute__ ((__packed__));
 ASSERT(sizeof(enum bot_orientation_t) == 1);
 
@@ -65,15 +135,27 @@ ASSERT(sizeof(enum visible_object_t) == 1);
 */
 
 enum field_of_view_t {
-	LEFT,
-	FRONT_LEFT,
-	FRONT,
-	FRONT_RIGHT,
-	RIGHT,
-	LEFT_AHEAD,
-	AHEAD,
-	RIGHT_AHEAD,
+
+	FIELD_OF_VIEW_LEFT,
+	FIELD_OF_VIEW_FRONT_LEFT,
+	FIELD_OF_VIEW_FRONT,
+	FIELD_OF_VIEW_FRONT_RIGHT,
+	FIELD_OF_VIEW_RIGHT,
+	FIELD_OF_VIEW_LEFT_AHEAD,
+	FIELD_OF_VIEW_AHEAD,
+	FIELD_OF_VIEW_RIGHT_AHEAD,
 	NR_FIELD_OF_VIEW,
+
+	/* Deprecated Symbols */
+	LEFT = FIELD_OF_VIEW_LEFT,
+	FRONT_LEFT = FIELD_OF_VIEW_FRONT_LEFT,
+	FRONT = FIELD_OF_VIEW_FRONT,
+	FRONT_RIGHT = FIELD_OF_VIEW_FRONT_RIGHT,
+	RIGHT = FIELD_OF_VIEW_RIGHT,
+	LEFT_AHEAD = FIELD_OF_VIEW_LEFT_AHEAD,
+	AHEAD = FIELD_OF_VIEW_AHEAD,
+	RIGHT_AHEAD = FIELD_OF_VIEW_RIGHT_AHEAD,
+
 } __attribute__ ((__packed__));
 ASSERT(sizeof(enum field_of_view_t) == 1);
 
